8322a571a2ecd0e25d7e57420f04aea09cda9fe5
listwas <listwa@patrykkr@gmail.com>
2026-01-28 14:12:21 +0100

    feat: add photo

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,10 +1,10 @@
 import styles from '../styles/ProductCard.module.css'
 
-export default function ProductCard({ product_title, product_price }) {
+export default function ProductCard({ product_title, product_price, product_image }) {
     return (
         <div className={styles.card_box}>
             <div className={styles.product_photo}>
-
+                <img src={product_image} alt='ps5 photo' />
             </div>
             <div className={styles.product_description}>
                 <h4> {product_title} </h4>
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import NavBar from '../components/NavBar'
 import ProductCard from '../components/ProductCard'
 import { Link } from 'react-router-dom'
 import '../styles/Homepage.css'
+import ps5Image from '../assets/ps5.jpg'
 
 export default function Homepage() {
     return (
@@ -9,7 +10,7 @@ export default function Homepage() {
             <NavBar page_header='Nasze Produkty' />
             <div className='product_cards'>
                 <Link to="product_details">
-                    <ProductCard product_title='product title exmaple' product_price='1234.99' />
+                    <ProductCard product_title='product title exmaple' product_price='1234.99' product_image={ps5Image} />
                 </Link>
                 <Link to="product_details">
                     <ProductCard product_title='product title exmaple' product_price='1234.99' />
